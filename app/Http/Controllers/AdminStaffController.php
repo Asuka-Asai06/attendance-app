d40c50a5879fcf2e79802112c518eec2f4fa7503
Asuka Asai <asaiasuka06@gmail.com>
2026-09-08 12:10:43 +0900

    docs: PHPDocコメントを追加

--- app/Http/Controllers/AdminStaffController.php
+++ app/Http/Controllers/AdminStaffController.php
@@ -17,7 +17,7 @@ class AdminStaffController extends Controller
     /**
      * スタッフ一覧を表示
      *
-     * @return View|Factory
+     * @return View スタッフ一覧ページ
      */
     public function index(): View
     {
@@ -26,6 +26,12 @@ class AdminStaffController extends Controller
         return view('admin.staff-list', compact('users'));
     }
 
+    /**
+     * スタッフ別勤怠一覧ページを表示
+     * @param Request $request
+     * @param User $user 表示対象のユーザー
+     * @return View スタッフ詳細ページ
+     */
     public function show(Request $request, User $user): View
     {
         $data = $this->adminAttendanceService->getUserMonthlyAttendance($user, $request->input('date'));
